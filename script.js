f20156a2df68c2059c334568912e9276b5670d64
barrysweeney <barrysweeney172@live.co.uk>
2020-03-04 22:06:16 +0000

    add time picker and bootstrap

--- script.js
+++ script.js
@@ -1,37 +1,77 @@
-const timeOn = document.getElementById("timeOn").innerHTML;
-const timeOff = document.getElementById("timeOff").innerHTML;
-const timeRemainingCounter = document.getElementById("timeRemaining");
-let id;
+let timeRemainingMinutesCounter = document.getElementById(
+  "timeRemainingMinutes"
+);
+let timeRemainingSecondsCounter = document.getElementById(
+  "timeRemainingSeconds"
+);
 
+let timeOn = document.getElementById("timeOn");
+let timeOff = document.getElementById("timeOff");
 
-startOnTimer();
+timeOn.addEventListener("input", checkIfReady);
+timeOff.addEventListener("input", checkIfReady);
 
-function startOnTimer(){
-let timeRemaining = parseInt(timeOn)
-id = setInterval(frame, 1000);
-  function frame() {
-    if(timeRemaining === 0 ){
-        timeRemainingCounter.innerHTML = 0;
-        clearInterval(id);
-        startOffTimer();
-    }else{
-          timeRemainingCounter.innerHTML = timeRemaining;
-          timeRemaining --;
-    }
-      }
+let startButton = document.getElementById("start");
+
+function checkIfReady() {
+  if (
+    parseInt(timeOn.value.split(":")[0]) >= 0 &&
+    parseInt(timeOn.value.split(":")[1]) >= 0 &&
+    parseInt(timeOff.value.split(":")[0]) >= 0 &&
+    parseInt(timeOff.value.split(":")[1]) >= 0
+  ) {
+    startButton.addEventListener("click", startOnTimer);
+  }
 }
 
-function startOffTimer(){
-    let timeRemaining = parseInt(timeOff)
-    id = setInterval(frame, 1000);
-      function frame() {
-        if(timeRemaining === 0 ){
-            timeRemainingCounter.innerHTML = 0;
-            clearInterval(id);
-            startOnTimer();
-        }else{
-              timeRemainingCounter.innerHTML = timeRemaining;
-              timeRemaining --;
-        }
-          }
+function startOnTimer() {
+  let timeOn = document.getElementById("timeOn");
+  let timeOnMinutes = timeOn.value.split(":")[0];
+  let timeOnSeconds = timeOn.value.split(":")[1];
+  let timeRemainingSeconds = parseInt(timeOnSeconds);
+  let timeRemainingMinutes = parseInt(timeOnMinutes);
+  let id = setInterval(frame, 1000);
+  function frame() {
+    if (timeRemainingMinutes === 0 && timeRemainingSeconds === 0) {
+      timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
+      timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
+      clearInterval(id);
+      startOffTimer();
+    } else if (timeRemainingSeconds === 0) {
+      timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
+      timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
+      timeRemainingMinutes--;
+      timeRemainingSeconds = 59;
+    } else {
+      timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
+      timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
+      timeRemainingSeconds--;
     }
+  }
+}
+
+function startOffTimer() {
+  let timeOff = document.getElementById("timeOff");
+  let timeOffMinutes = timeOff.value.split(":")[0];
+  let timeOffSeconds = timeOff.value.split(":")[1];
+  let timeRemainingSeconds = parseInt(timeOffSeconds);
+  let timeRemainingMinutes = parseInt(timeOffMinutes);
+  let id = setInterval(frame, 1000);
+  function frame() {
+    if (timeRemainingMinutes === 0 && timeRemainingSeconds === 0) {
+      timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
+      timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
+      clearInterval(id);
+      startOnTimer();
+    } else if (timeRemainingSeconds === 0) {
+      timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
+      timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
+      timeRemainingMinutes--;
+      timeRemainingSeconds = 59;
+    } else {
+      timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
+      timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
+      timeRemainingSeconds--;
+    }
+  }
+}
