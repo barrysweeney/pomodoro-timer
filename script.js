80fbf1c01d79dfc04dd49abe03c30a8419dd9e68
barrysweeney <barrysweeney172@live.co.uk>
2020-03-05 16:58:09 +0000

    add pause button and pause functionality

--- script.js
+++ script.js
@@ -5,13 +5,36 @@ let timeRemainingSecondsCounter = document.getElementById(
   "timeRemainingSeconds"
 );
 
-let timeOn = document.getElementById("timeOn");
-let timeOff = document.getElementById("timeOff");
+let isOn = false;
+let isOff = false;
+let timesPaused = 0;
+
+let paused = false;
+
+let startButton = document.getElementById("start");
+let pauseButton = document.getElementById("pause");
+let stopButton = document.getElementById("stop");
 
 timeOn.addEventListener("input", checkIfReady);
 timeOff.addEventListener("input", checkIfReady);
 
-let startButton = document.getElementById("start");
+function pause() {
+  timesPaused++;
+  if (paused === false) {
+    paused = true;
+    pauseButton.innerHTML = "Resume";
+    startButton.addEventListener("click", startOnTimer);
+  } else {
+    paused = false;
+    pauseButton.innerHTML = "Pause";
+    if (isOn === true) {
+      startOnTimer();
+    }
+    if (isOff === true) {
+      startOffTimer();
+    }
+  }
+}
 
 function checkIfReady() {
   if (
@@ -20,22 +43,26 @@ function checkIfReady() {
     parseInt(timeOff.value.split(":")[0]) >= 0 &&
     parseInt(timeOff.value.split(":")[1]) >= 0
   ) {
+    setInitialOnConditions();
     startButton.addEventListener("click", startOnTimer);
   }
 }
 
 function startOnTimer() {
-  let timeOn = document.getElementById("timeOn");
-  let timeOnMinutes = timeOn.value.split(":")[0];
-  let timeOnSeconds = timeOn.value.split(":")[1];
-  let timeRemainingSeconds = parseInt(timeOnSeconds);
-  let timeRemainingMinutes = parseInt(timeOnMinutes);
+  startButton.removeEventListener("click", startOnTimer);
+  isOff = false;
+  isOn = true;
+  pauseButton.addEventListener("click", pause);
+
   let id = setInterval(frame, 1000);
   function frame() {
-    if (timeRemainingMinutes === 0 && timeRemainingSeconds === 0) {
+    if (paused === true) {
+      clearInterval(id);
+    } else if (timeRemainingMinutes === 0 && timeRemainingSeconds === 0) {
       timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
       timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
       clearInterval(id);
+      setInitialOffConditions();
       startOffTimer();
     } else if (timeRemainingSeconds === 0) {
       timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
@@ -50,18 +77,37 @@ function startOnTimer() {
   }
 }
 
-function startOffTimer() {
+function setInitialOnConditions() {
+  let timeOn = document.getElementById("timeOn");
+  timeOnMinutes = timeOn.value.split(":")[0];
+  timeOnSeconds = timeOn.value.split(":")[1];
+  timeRemainingSeconds = parseInt(timeOnSeconds);
+  timeRemainingMinutes = parseInt(timeOnMinutes);
+}
+
+function setInitialOffConditions() {
   let timeOff = document.getElementById("timeOff");
-  let timeOffMinutes = timeOff.value.split(":")[0];
-  let timeOffSeconds = timeOff.value.split(":")[1];
-  let timeRemainingSeconds = parseInt(timeOffSeconds);
-  let timeRemainingMinutes = parseInt(timeOffMinutes);
+  timeOffMinutes = timeOff.value.split(":")[0];
+  timeOffSeconds = timeOff.value.split(":")[1];
+  timeRemainingSeconds = parseInt(timeOffSeconds);
+  timeRemainingMinutes = parseInt(timeOffMinutes);
+}
+
+function startOffTimer() {
+  isOn = false;
+  isOff = true;
+
   let id = setInterval(frame, 1000);
   function frame() {
-    if (timeRemainingMinutes === 0 && timeRemainingSeconds === 0) {
+    if (paused === true) {
+      clearInterval(id);
+    } else if (timeRemainingMinutes === 0 && timeRemainingSeconds === 0) {
       timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
       timeRemainingSecondsCounter.innerHTML = timeRemainingSeconds;
       clearInterval(id);
+      timeRemainingSeconds = parseInt(timeOnSeconds);
+      timeRemainingMinutes = parseInt(timeOnMinutes);
+      setInitialOnConditions();
       startOnTimer();
     } else if (timeRemainingSeconds === 0) {
       timeRemainingMinutesCounter.innerHTML = timeRemainingMinutes;
